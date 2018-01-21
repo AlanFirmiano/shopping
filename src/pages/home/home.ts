bb3b12af50281b37eae82dbd5de33f178e7e2c2b
alanfirmiano <alanfirmiano18@gmail.com>
2018-01-21 12:38:54 -0300

    Mostrando list itens

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {IonicPage, NavController} from 'ionic-angular';
+import {ShoppingListService} from "../../services/shopping-list/shopping-list.service";
+import {Observable} from "rxjs/Observable";
+import {Item} from "../../models/item/item.module";
 
 @IonicPage()
 @Component({
@@ -8,8 +11,25 @@ import {IonicPage, NavController} from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  shoppingList$: Observable<Item[]>;
 
+  constructor(
+    public navCtrl: NavController,
+    private shopping: ShoppingListService
+  ) {
+    this.shoppingList$ = this.shopping
+      .getShoppingList() //DB LIST
+      .snapshotChanges() //KEY - VALUE
+      .map(changes=>{
+      return changes.map(c=>({
+        key: c.payload.key,
+        ...c.payload.val(),
+      }));
+    });
   }
+// {
+//   key: 'value-here',
+//   nome: 'Notebook'
+// }
 
 }
