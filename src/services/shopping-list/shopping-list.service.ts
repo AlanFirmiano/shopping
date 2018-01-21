b2ac13d743b5e1ce0ec3825971fb0f92ca73ce9a
alanfirmiano <alanfirmiano18@gmail.com>
2018-01-21 11:58:31 -0300

    metodo getShoppingList

---
+++ src/services/shopping-list/shopping-list.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from "@angular/core";
+import {AngularFireDatabase} from "angularfire2/database";
+import {Item} from "../../models/item/item.module";
+
+@Injectable()
+export class ShoppingListService{
+
+  private shoppingListRef = this.db.list<Item>('shopping-list');
+
+  constructor(private db: AngularFireDatabase){}
+
+  getShoppingList(){
+    return this.shoppingListRef;
+  }
+}
